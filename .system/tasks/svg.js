8ad487958505c9199928ea4810f8a302c0626cb3
slavikse <gslavikse@gmail.com>
2016-06-12 13:31:19 +0300

    Добавлена задача для сборки спрайта из svg

---
+++ .system/tasks/svg.js
@@ -0,0 +1,41 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import rename from 'gulp-rename';
+import svgSprite from 'gulp-svg-sprite';
+import notify from '../utility/notify';
+import watch from '../utility/watch';
+
+const
+  NAME = 'svg',
+  FILES = [
+    'source/**/svg/*',
+    '!source/**/{_*/**,_*}'
+  ],
+  THERE = 'public/svg',
+  CONFIG = {
+    mode: {
+      symbol: {
+        sprite: 'sprite.svg',
+        dest: ''
+      }
+    },
+    svg: {
+      xmlDeclaration: false,
+      doctypeDeclaration: false
+    }
+  };
+
+/**
+ * Создаем спрайт из svg
+ */
+export default () => {
+  watch(NAME, FILES);
+
+  gulp.task(NAME, () => {
+    return gulp.src(FILES)
+      .pipe(plumber({errorHandler: notify}))
+      .pipe(rename({dirname: ''}))
+      .pipe(svgSprite(CONFIG))
+      .pipe(gulp.dest(THERE))
+  })
+}
