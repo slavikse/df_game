625168ca1672057e5b15ad07fe402ea9902846eb
slavikse <gslavikse@gmail.com>
2016-06-12 13:24:14 +0300

    Добавлена задача для перемещения изображений.

---
+++ .system/tasks/image.js
@@ -0,0 +1,26 @@
+import gulp from 'gulp';
+import rename from 'gulp-rename';
+import watch from '../utility/watch';
+
+const
+  NAME = 'image',
+  FILES = [
+    'source/**/image/*',
+    '!source/**/{_*/**,_*}'
+  ],
+  THERE = 'public/image';
+
+/**
+ * Перемещает картинки
+ */
+export default () => {
+  watch(NAME, FILES);
+
+  gulp.task(NAME, () => {
+    return gulp.src(FILES, {
+      since: gulp.lastRun(NAME)
+    })
+      .pipe(rename({dirname: ''}))
+      .pipe(gulp.dest(THERE))
+  })
+}
