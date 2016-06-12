659b82fa4d860260e1f92afc8d92087608e2efdc
slavikse <gslavikse@gmail.com>
2016-06-12 14:40:57 +0300

    Добавлена задача для сборки стилей (postcss)

--- .system/run.js
+++ .system/run.js
@@ -1,13 +1,21 @@
 import del from './tasks/del';
 import font from './tasks/font';
-import sprite from './tasks/sprite';
-import view from './tasks/view';
+import image from './tasks/image';
 import lazyLib from './tasks/lazy_lib';
+import resize from './tasks/resize';
+import sprite from './tasks/sprite';
+import style from './tasks/style';
+import svg from './tasks/svg';
+import view from './tasks/view';
 
 export default () => {
   del();
   font();
-  sprite();
-  view();
+  image();
   lazyLib();
+  resize();
+  sprite();
+  style();
+  svg();
+  view();
 }
---
+++ .system/tasks/style.js
@@ -0,0 +1,43 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import postcss from 'gulp-postcss';
+import media from 'postcss-media-minmax';
+import nesting from 'postcss-nesting';
+import notify from '../utility/notify';
+import watch from '../utility/watch';
+
+const
+  NAME = 'style',
+  FILES = [
+    'source/main.css',
+    'source/**/*.css',
+    '!source/**/{lib/*,lazy/*,_*/**,_*}'
+  ],
+  THERE = 'public';
+
+/**
+ * Собираем стили
+ */
+export default () => {
+  watch(NAME, FILES);
+
+  gulp.task(NAME, () => {
+    return gulp.src(FILES)
+      .pipe(plumber({errorHandler: notify}))
+      .pipe(postcss([
+        media,
+        nesting
+      ]))
+      .pipe(gulp.dest(THERE))
+  })
+}
+
+/* Syntax:
+ * @media (500px <= width <= 1200px)
+ * nesting: a & b
+ * */
+
+/*TODO
+  var autoprefixer = require('autoprefixer');
+  var cssnano = require('cssnano');
+*/
