7205c986f6acfd162e62081bf674edaf37acbe82
Вячеслав <gslavikse@gmail.com>
2016-10-04 22:56:39 +0300

    Дополнел useful

--- source/index.js
+++ source/index.js
@@ -5,22 +5,3 @@ import './start_screen/start_screen';
 import './result/result';
 
 // загрузив игру, создаст событие, которое слушает стартовый экран
-
-
-/** Support external SVG: IE 9-11 && Edge 12- */
-// import svg4everybody from 'libs/svg4everybody';
-// svg4everybody();
-
-/**
- * A responsive sprite polyfill for <picture>, srcset, sizes: IE11- [~12kb minify]
- * При использовании webp поправить конфиг в сборке изображений
- */
-// import picturefill from 'picturefill';
-// picturefill();
-
-/*
- import template from 'underscore.template';
- template('Hello, <%= name %>')({name: 'Paul'});
- var list = "<% _.each(people, function(name) { %> <li><%= name %></li> <% }); %>";
- template(list, {people : ['moe', 'curly', 'larry']}); // вернёт "<li>moe</li><li>curly</li><li>larry</li>"
- */
