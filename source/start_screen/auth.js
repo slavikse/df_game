e7f2dd9cc870a1643a98c655c3df40fe5f32abc1
Вячеслав <gslavikse@gmail.com>
2016-10-24 02:20:09 +0300

    game

--- source/start_screen/auth.js
+++ source/start_screen/auth.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app.js';
+import firebase from 'firebase/app';
 import 'firebase/auth.js';
 // import 'firebase/database';
 // import 'firebase/storage';
@@ -6,6 +6,8 @@ import 'firebase/auth.js';
 const
   $body = document.body,
   $startScreen = $body.querySelector('.start-screen'),
+  $authWrap = $startScreen.querySelector('.auth-wrap'),
+  $authShowToggle = $startScreen.querySelector('.auth-show-toggle'),
   $login = $startScreen.querySelector('.login'),
   $submit = $startScreen.querySelector('.submit'),
 
@@ -16,6 +18,10 @@ const
     storageBucket: "dark-forest-1567c.appspot.com",
     messagingSenderId: "877244196821"
   };
+
+let
+  isAuthShow = false,
+  loginSave;
 
 firebase.initializeApp(config);
 
@@ -29,18 +35,24 @@ function auth(e) {
     login = authElem.login.value,
     password = authElem.password.value;
 
+  loginSave = login;
   fbAuth(login, password);
 }
 
 function fbAuth(login, password) {
-  const unrealEmail = `${login}@gmail.com`;
-
   firebase.auth()
-  .createUserWithEmailAndPassword(unrealEmail, password)
-  .then(e => {
-    console.log(e)
-  })
+  .createUserWithEmailAndPassword(login, password)
+  .then(authSuccess)
   .catch(authError);
+}
+
+function authSuccess(auth) {
+  window.uid = auth.uid;
+
+  authShowToggle();
+  $authShowToggle.textContent = `Привет ${loginSave}!`;
+
+  console.log(auth.uid);
 }
 
 function authError(error) {
@@ -51,5 +63,19 @@ function authError(error) {
 //
 // }
 
+function authShowToggle() {
+  if (isAuthShow) {
+    isAuthShow = false;
+    $authShowToggle.textContent = 'Вход';
+  } else {
+    isAuthShow = true;
+    $authShowToggle.textContent = 'Закрыть';
+  }
+
+  $authShowToggle.classList.toggle('auth-show-toggle-down');
+  $authWrap.classList.toggle('auth-wrap-show');
+}
+
 $submit.addEventListener('click', auth);
 document.addEventListener('auth', auth);
+$authShowToggle.addEventListener('click', authShowToggle);
