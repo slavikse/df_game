efc0dfcc55190ef66e68a307cd213b8e0e777a94
slavikse <gslavikse@gmail.com>
2016-12-21 14:48:35 +0300

    game

--- martyr/utility/row.sh
+++ martyr/utility/row.sh
@@ -4,10 +4,10 @@ view=$(find source -name '*.html' -type f -print0 | xargs -0 cat | wc -l) &&
 style=$(find source -name '*.css' -type f -print0 | xargs -0 cat | wc -l) &&
 script=$(find source -name '*.js' -type f -print0 | xargs -0 cat | wc -l) &&
 
-echo && echo '»»»»»»' && echo &&
-echo "view: ${view}" &&
-echo "style: ${style}" &&
-echo "script: ${script}" &&
-echo '____________' &&
-echo "TOTAL: $(($view+$style+$script))" &&
-echo && echo '»»»»»»' && echo ''
+echo &&
+echo "  view | ${view}" &&
+echo " style | ${style}" &&
+echo "script | ${script}" &&
+echo '———————⟊—————' &&
+echo " TOTAL | $(($view+$style+$script))" &&
+echo ''
